f3086799d535f1925a62e5ffa0d355e19598d739
dominikfoldi <dominik.foldi@outlook.com>
2022-04-05 20:30:19 +0200

    Add d3 and implement csv load

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,3 +1,15 @@
-export default async function main() {
-  // something here
+#!/usr/bin/env node
+
+import api from "../server"
+
+async function main() {
+  console.log(await api());
 }
+
+(async () => {
+  try {
+    await main();
+  } catch (error) {
+    console.log(error);
+  }
+})();
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,15 @@
+import * as path from "path";
+import { fileURLToPath } from 'url';
+import * as fs from "fs";
+import * as d3 from "d3";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default async function main() {
-  // something here
+  const csvPath = path.resolve(__dirname, "../../data/instagram_influencers.csv");
+  const csvData = fs.readFileSync(csvPath).toString();
+  const influencers = d3.csvParse(csvData);
+
+  return influencers;
 }
